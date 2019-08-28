f644ab13737f9d19d7564232f343953066b2380e
Kim Lewis <kalewi2@gmail.com>
2019-08-28 15:15:34 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
 # Add your variables here
+def first_number(10)
+def second_number(20)
+#def sum(first_number + second_number)
+#def subtract(second_number - first_number)
+#def multiply(first_number * second_number)
+#def divide(second_number / first_number)
+end
